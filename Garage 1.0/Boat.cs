8a5d5ccc1ea09bd9a73c69d922a7c1964e75bd2d
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-28 09:06:34 +0200

    fil2

---
+++ Garage 1.0/Boat.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_1._0
+{
+    class Boat:Vehicle
+    {
+        public string Typ { get; set; }
+        public int MaxNumberOfPassengers { get; set; }
+
+        public Boat(string typ, int maxNumberOfPassenger, string regnr, string colour, int numberOfWheel)
+        {
+            Typ = typ;
+            MaxNumberOfPassengers = maxNumberOfPassenger;
+            Regnr = regnr;
+            Colour = colour;
+            NumberOfWheels = numberOfWheel;           
+        }
+
+        public override string ToString()
+        {
+            return ("Båt:       " + " Regnr:" + this.Regnr + ", Färg:" + this.Colour + ", Hjul:" + this.NumberOfWheels + ", Modell:" + this.Typ + ", Passagerare:" + this.MaxNumberOfPassengers);
+        }
+    }
+
+    
+}
