b58f8c6db62932d6ab24632adde38f50ba08b7ad
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-28 09:05:35 +0200

    fil1

---
+++ Garage 1.0/Airplane.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_1._0
+{
+    class Airplane:Vehicle
+    {
+        public int NumberOfWings { get; set; }
+        public int NumberOfPropeller { get; set; }
+
+        public Airplane(int numberOfWings, int numberOfPropeller, string regnr, string colour, int numberOfWheel)
+        {
+            NumberOfWings = numberOfWings;
+            NumberOfPropeller = numberOfPropeller;
+            Regnr = regnr;
+            Colour = colour;
+            NumberOfWheels = numberOfWheel;           
+        }
+
+        public override string ToString()
+        {
+            return ("Flygplan:  " + " Regnr:" + this.Regnr + ", Färg:" + this.Colour + ", Hjul:" + this.NumberOfWheels + ", Vingar:" + this.NumberOfWings + " Propellrar:" + this.NumberOfPropeller);
+        }
+
+    }
+}
