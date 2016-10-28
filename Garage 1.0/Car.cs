db96e98703caed5bda0d3e2bd0cbd44b3d709522
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-28 09:07:27 +0200

    fil4

---
+++ Garage 1.0/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_1._0
+{
+    class Car:Vehicle
+    {
+        public string Label { get; set; }
+        public int KmHour { get; set; }
+
+        public Car(string label, int kmHour, string regnr, string colour, int numberOfWheel)
+        {
+            Label = label;
+            KmHour = kmHour;
+            Regnr = regnr;
+            Colour = colour;
+            NumberOfWheels = numberOfWheel;           
+        }
+
+        public override string ToString()
+        {
+            return ("Bil:       " + " Regnr:" + this.Regnr + ", Färg:" + this.Colour + ", Hjul:" + this.NumberOfWheels + ", Märke:" + this.Label + ", Hastighet:" + this.KmHour + "k/h");
+        }
+    }
+}
