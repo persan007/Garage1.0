58015a8c38f1790eefd92ecffcbc42fad7ddb2f5
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-28 09:07:04 +0200

    fil3

---
+++ Garage 1.0/Buss.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_1._0
+{
+    class Buss:Vehicle
+    {
+        public int Length { get; set; }
+        public int Height { get; set; }
+
+        public Buss(int lenght, int height, string regnr, string colour, int numberOfWheel)
+        {
+            Length = lenght;
+            Height = height;
+            Regnr = regnr;
+            Colour = colour;
+            NumberOfWheels = numberOfWheel;           
+        }
+
+        public override string ToString()
+        {
+            return ("Buss:      " + " Regnr:" + this.Regnr + ", Färg:" + this.Colour + ", Hjul:" + this.NumberOfWheels + ", Längd:" + this.Length + ", Höjd:" + this.Height);
+        }
+    }
+}
