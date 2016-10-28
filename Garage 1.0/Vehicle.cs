201f935da518b72f755cf77a0de74cf34f6795aa
persan007 <per-ake.sandstrom@hotmail.se>
2016-10-28 09:09:08 +0200

    fil8

---
+++ Garage 1.0/Vehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_1._0
+{
+    class Vehicle
+    {
+        public string Regnr { get; set; }
+        public string Colour { get; set; }
+        public int NumberOfWheels { get; set; }        
+    }
+}
